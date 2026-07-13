86e3ef484115066a0853c45f4acb58b6addaf36a
ok67sa-alt <ok67sa@gmail.com>
2026-07-13 02:37:08 +0200

    Remove duplicate pages/api/api and pages/admin/admin directories

--- pages/api/api/auth/logout.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import { removeTokenCookie } from '../../../lib/auth';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  removeTokenCookie(res);
-  res.status(200).json({ loggedOut: true });
-}
